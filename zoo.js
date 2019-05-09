58ea2685db31847ce2c1f8db1839f14b56fbe553
Dimer <lemstra@gmail.com>
2019-05-09 16:56:46 +0200

    More code, working on arrays,  i might however change the approach and use a more modern notation with classes.

--- zoo.js
+++ zoo.js
@@ -1,3 +1,10 @@
+////////////////////////// DEFINING GLOBAL VARIABLES  \\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+let animalsInWorld = [];
+
+
+///////////////////////////// DEFINING ANIMALS  \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
 function ZooAnimals() { 
 
 }
@@ -18,10 +25,14 @@ ZooAnimals.prototype = {
     }
 };
 
-function Birds(species, sex) { 
+function Birds(uid, type, species, sex, age, name) { 
 
+    this.uid = uid;
+    this.type = Birds;
     this.species = species
     this.sex = sex;
+    this.age = age;
+    this.name = name;
     this.numLegs = 2;
     this.canFly = true;
           
@@ -30,12 +41,7 @@ function Birds(species, sex) {
 Birds.prototype = Object.create(ZooAnimals.prototype);
 Birds.prototype.constructor = Birds;
 
-
-let Gull1 = new Birds("gull", "male");
-let Gull2 = new Birds("gull", "female");
-
-let Swallow1 = new Birds("swallow", "female");
-
+//for (let animal in animalsInWorld) {...};
 
 
 function Mamals() { };
@@ -56,8 +62,6 @@ Reptiles.prototype = Object.create(ZooAnimals.prototype);
 Reptiles.prototype.constructor = Reptiles;
 
 
-
-
 //console.log(Birds.constructor === ZooAnimals); // => true
 
  
@@ -67,7 +71,7 @@ ZooObjects.prototype = {
     constructor: ZooObjects,
 
     describe: function() {
-        console.log("My name is " + this.name + " and im a " + this.prototype);
+        console.log("My name is " + this.name + " and im a " + this.sex + " " + this.species);
     }
 };
 
@@ -77,24 +81,35 @@ ZooPersonel.prototype = {
     constructor: ZooPersonel,
 
     describe: function() {
-        console.log("My name is " + this.name + " and im a " + this.prototype);
+        console.log("My name is " + this.name + " and im a " + this.sex + " " + this.species);
     }
 };
 
 
 
-Gull1.eat();
-Gull1.describe();
+////////////////////////////////// CODE EXECUTION \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+CreateAnimal("1", Birds, "Gull", "Male", "2", "Bob");
+CreateAnimal("2", Birds, "Gull", "Female", "4", "Jessica");
+CreateAnimal("3", Birds, "Swallow", "Female", "3", "Tiffany");
+
+function CreateAnimal(uid, type, species, sex, age, name) {
+
+animalsInWorld[uid, type, species, sex, age, name] = new type(uid, species, sex, age, name);
+
+};
+
+var newArray = animalsInWorld.map( function( el ){ 
+    return el.name;
+
+   });
+
+console.log(animalsInWorld);
+//animalsInWorld.toString();
+//console.log(animalsInWorld);
 
 
-let ownProps = [];
-
-for (let property in Gull1) {
-  if(Gull1.hasOwnProperty(property)) {
-    ownProps.push(property);
-  }
+for (var an in animalsInWorld)
+{
+    document.getElementsByClassName("box")[0].innerHTML += an;
+    console.log(an);
 }
-
-console.log(ownProps); // prints [ "name", "numLegs" ]
-
-
